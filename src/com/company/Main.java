4a317034d1e1cc3f3ec97db742153c97bd3d3914
Venki Gandra <gandravenkatesh@gmail.com>
2020-08-05 16:32:27 -0700

    working code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,21 @@
 package com.company;
 
+/*import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;*/
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.*;
 
 public class Main {
 
     private static HttpURLConnection connection;
-    public static  void main(String[] args){
+    public static  void main(String[] args) {
         // method 1: java.net.HttpURLConnection
         BufferedReader reader;
         String line;
@@ -29,27 +34,50 @@ public class Main {
 
             if (status > 200) {
                 reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-                while( (line = reader.readLine()) != null){
+                while ((line = reader.readLine()) != null) {
                     responseContent.append(line);
                 }
                 reader.close();
             } else {
                 reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                while((line = reader.readLine()) != null) {
+                while ((line = reader.readLine()) != null) {
                     responseContent.append(line);
                 }
                 reader.close();
             }
-            System.out.println(responseContent.toString());
+           // System.out.println(responseContent.toString());
+            parse(responseContent.toString());
 
-        }
-        catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             connection.disconnect();
         }
+
+
+        // Method 2: java.net.http.HttpClient
+       /* HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://jsonplaceholder.typicode.com/albums")).build();
+        client.sendAsync(request, HttpResponse.BodyHandler.asString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out.println)
+                .join();*/
+    }
+        public static String parse(String responseBody){
+            JSONArray albums = new JSONArray(responseBody);
+            for (int i =0; i < albums.length(); i++) {
+                JSONObject album = albums.getJSONObject(i);
+                int id = album.getInt("id");
+                int userid = album.getInt("userId");
+                String title = album.getString("title");
+                System.out.println(id + " " + title + " " + userid);
+
+            }
+            return null;
+
+
 
     }
 }
