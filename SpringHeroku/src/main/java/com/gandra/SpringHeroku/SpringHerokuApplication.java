b561a560463b6b27332f5e19925cc201c262e2a6
Gandra <gandravenkatesh@gmail.com>
2020-08-10 21:00:41 -0700

    working code

---
+++ SpringHeroku/src/main/java/com/gandra/SpringHeroku/SpringHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.gandra.SpringHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuApplication.class, args);
+	}
+
+}
---
+++ SpringHeroku/src/test/java/com/gandra/SpringHeroku/SpringHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gandra.SpringHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
